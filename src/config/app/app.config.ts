1f17b4b7e9f7660f53c9240218779bdc151688ad
Chris-AM <c.aranguizm@outlook.com>
2024-02-29 03:23:10 -0300

    :bricks: allowing Http Transactions

--- src/config/app/app.config.ts
+++ src/config/app/app.config.ts
@@ -2,9 +2,23 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { routes } from '../routes/app.routes';
+import { ErrorResponseInterceptor } from '../../infrastructure/interceptors/error-response.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([ErrorResponseInterceptor])
+    ),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+  ],
 };
---
+++ src/config/environment.config.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  links: {
+    base_url: 'http://localhost:3000',
+    paths: '/links',
+  },
+  expernal_links: {
+    base_url: 'https://dummyjson.com',
+    clients: {
+      base_url: '/users',
+    },
+    products: {
+      base_url: '/products',
+    },
+  },
+};
