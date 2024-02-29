4e710c0721fbc1a016616ee3a5fa59e6468a5959
Chris-AM <c.aranguizm@outlook.com>
2024-02-29 11:38:28 -0300

    :sparkles: app toolbar ready

--- src/app/presentation/shared/tool-bar/tool-bar.component.ts
+++ src/app/presentation/shared/tool-bar/tool-bar.component.ts
@@ -8,17 +8,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { LinksComponent } from '../links/links.component';
 import { GetCodeComponent } from '../get-code/get-code.component';
 import { ThemeComponent } from '../theme/theme.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tool-bar',
   standalone: true,
-  imports: [MatToolbarModule, LinksComponent, GetCodeComponent, ThemeComponent],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    LinksComponent,
+    GetCodeComponent,
+    ThemeComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './tool-bar.component.html',
   styleUrl: './tool-bar.component.css',
 })
 export class ToolBarComponent implements OnInit {
-  public title = signal<String>('');
+  public title = signal<string>('');
+  public toggleMenu = signal<boolean>(false);
 
   ngOnInit(): void {
     this.onTitleChange();
@@ -26,5 +36,9 @@ export class ToolBarComponent implements OnInit {
 
   public onTitleChange(): void {
     this.title.set('Tienda Electrónica');
+  }
+
+  public onToggleMenu(): void {
+    this.toggleMenu.set(!this.toggleMenu().valueOf());
   }
 }
