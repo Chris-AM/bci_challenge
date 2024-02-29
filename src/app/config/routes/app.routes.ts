f84f42219b78f466a675509b4b09552438668440
Chris-AM <c.aranguizm@outlook.com>
2024-02-29 14:47:12 -0300

    :bricks: declaring basic app routes

--- src/app/config/routes/app.routes.ts
+++ src/app/config/routes/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', loadComponent: () => import('../../presentation/pages/home/home.component')},
+  { path: 'clients', loadComponent: () => import('../../presentation/pages/clients/clients.component') },
+  { path: 'products', loadComponent: () => import('../../presentation/pages/products/products.component')},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
